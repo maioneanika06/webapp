c4e593198e7e10fa06fb7bad5892a81115922248
Maione Reyes <anikareyes264@gmail.com>
2026-05-19 14:46:26 +0800

    link line 23-24

--- src/app/api/notify-staff/route.ts
+++ src/app/api/notify-staff/route.ts
@@ -20,8 +20,8 @@ export async function POST(req: Request) {
             },
         });
 
-        const adminUrl = process.env.NEXT_PUBLIC_ADMIN_URL || "http://vendywebapp.vercel.app";
-        const registrationUrl = process.env.NEXT_PUBLIC_REGISTRATION_URL || "http://vendyregistrationform.vercel.app";
+        const adminUrl = process.env.NEXT_PUBLIC_ADMIN_URL || "https://vendywebapp.vercel.app";
+        const registrationUrl = process.env.NEXT_PUBLIC_REGISTRATION_URL || "https://vendyregistrationform.vercel.app";
 
         const mailOptions = {
             from: process.env.GMAIL_USER,
